b2998b5a0aa687ec5f99f7f8403607878d6d9c28
mohammedRasif <mdrasif3499@gmail.com>
2025-05-23 22:04:18 +0600

    crate some new

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,4 +1,4 @@
-import {createBrowserRouter} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
 import Main from "../Layout/Main";
 import Home from "../Pages/Home/Home";
 import Registration from "../Pages/Authentication/Registration";
@@ -8,29 +8,29 @@ import EmailVerification from "../Pages/Authentication/EmailVerification";
 import OTP_Verification from "../Pages/Authentication/OTP_Verification";
 import ResetPassword from "../Pages/Authentication/ResetPassword";
 import Admin_Home from "../Layout/Admin/Admin_Home";
-  
+
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Main />,
-        children: [
-          {path: '/', element: <Home/>}
-      ]
-    },
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      { path: '/', element: <Home /> }
+    ]
+  },
 
-    {
-        path: '/dashboard',
-        element: <DashboardLayout />,
-      children: [
-      {index: true, element: <Admin_Home />},
-      {path: 'admin_home', element: <Admin_Home/>}
-        ]
-    },
+  {
+    path: '/dashboard',
+    element: <DashboardLayout />,
+    children: [
+      { index: true, element: <Admin_Home /> },
+      { path: 'admin_home', element: <Admin_Home /> }
+    ]
+  },
 
-    {path: '/sign_up', element: <Registration/>},
-    {path: '/login', element: <Login/>},
-    {path: '/verify', element: <EmailVerification/>},
-    {path: '/otp_verify', element: <OTP_Verification/>},
-    {path: '/reset_password', element: <ResetPassword/>},
-  ]);
+  { path: '/sign_up', element: <Registration /> },
+  { path: '/login', element: <Login /> },
+  { path: '/verify', element: <EmailVerification /> },
+  { path: '/otp_verify', element: <OTP_Verification /> },
+  { path: '/reset_password', element: <ResetPassword /> },
+]);
