d3d67dc4e0ccbf7323a1c782d64b7f14734765b8
saminyasar004 <yasarsamin57@gmail.com>
2025-06-05 20:32:57 +0600

    fix: homelayout responsive added

--- src/Layout/User/index.jsx
+++ src/Layout/User/index.jsx
@@ -1,6 +1,120 @@
+// import { Plus } from "lucide-react";
+// import { Button } from "@/components/ui/button";
+// import CreatedPlanCard from "../../components/created-plan-card";
+// import { Link, NavLink, useLocation } from "react-router-dom";
+// import { useEffect } from "react";
+
+// const HomeLayout = ({ children }) => {
+// 	const { pathname } = useLocation();
+
+// 	return (
+// 		<div className="w-full h-auto">
+// 			<div className="w-full flex flex-col gap-2 px-4">
+// 				<h2 className="text-[#343E4B] font-semibold text-3xl">
+// 					Welcome,{" "}
+// 					<span className="font-normal ">
+// 						Publish your plan to get perfect offer
+// 					</span>
+// 				</h2>
+// 			</div>
+
+// 			<div className="w-full grid grid-cols-8 py-8">
+// 				<div className="col-span-6">
+// 					<div className="w-full flex items-end justify-end">
+// 						<NavLink to="/user/CreatePlan">
+// 						<Button
+// 							variant="transparent"
+// 							className="font-nunito-sans font-semibold"
+// 						>
+// 							Create Plan
+// 							<Plus size={18} />
+// 						</Button>
+// 						</NavLink>
+// 					</div>
+
+// 					<div className="w-full flex items-center gap-2 px-4">
+// 						<div className="min-w-max">
+// 							<h4 className="text-base font-semibold text-[#343E4B]">
+// 								12 July, 2025
+// 							</h4>
+// 						</div>
+
+// 						<div className="w-full h-[1px] bg-[#B8BABD]"></div>
+// 					</div>
+
+// 					<div>{children}</div>
+// 				</div>
+
+// 				<div className="col-span-2 pt-16 mx-10">
+// 					<h3 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
+// 						My All Plans
+// 					</h3>
+// 					<div className="flex flex-col gap-4 py-2 px-4">
+// 						<Link to="/user">
+// 							<button
+// 							className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
+// 										pathname === "/user" ? "bg-white shadow-md"
+// 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
+// 								}`}
+// 							>
+// 								Created Plan
+// 							</button>
+// 						</Link>
+// 						<Link to="/user/published">
+// 							<button
+// 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
+// 										pathname === "/user/published" ? "bg-white shadow-md"
+// 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
+// 								}`}
+// 							>
+// 								Published Plans
+// 							</button>
+// 						</Link>
+// 						<Link to="/user/accepted">
+// 							<button
+// 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
+// 										pathname === "/user/accepted" ? "bg-white shadow-md"
+// 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
+// 								}`}
+// 							>
+// 								Accepted Offers
+// 							</button>
+// 						</Link>
+// 						<Link to="/user/favourite">
+// 							<button
+// 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
+// 										pathname === "/user/favourite" ? "bg-white shadow-md"
+// 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
+// 								}`}
+// 							>
+
+// 								Favourite Agencies
+// 							</button>
+// 						</Link>
+
+// 						<div className="flex flex-col gap-1">
+// 							<p className="text-sm text-gray-900 font-semibold mb-2">
+// 								Need free fasted response?
+// 							</p>
+// 							<Button
+// 								variant="link"
+// 								size="sm"
+// 								className="p-0 underline text-left w-min text-xs"
+// 							>
+// 								Click here
+// 							</Button>
+// 						</div>
+// 					</div>
+// 				</div>
+// 			</div>
+// 		</div>
+// 	);
+// };
+
+// export default HomeLayout;
+
 import { Plus } from "lucide-react";
 import { Button } from "@/components/ui/button";
-import CreatedPlanCard from "../../components/created-plan-card";
 import { Link, NavLink, useLocation } from "react-router-dom";
 import { useEffect } from "react";
 
@@ -12,23 +126,24 @@ const HomeLayout = ({ children }) => {
 			<div className="w-full flex flex-col gap-2 px-4">
 				<h2 className="text-[#343E4B] font-semibold text-3xl">
 					Welcome,{" "}
-					<span className="font-normal ">
+					<span className="font-normal">
 						Publish your plan to get perfect offer
 					</span>
 				</h2>
 			</div>
 
-			<div className="w-full grid grid-cols-8 py-8">
-				<div className="col-span-6">
+			<div className="w-full flex flex-col max-lg:gap-4 lg:grid lg:grid-cols-8 lg:py-8">
+				{/* Main Content Section */}
+				<div className="lg:col-span-6 max-lg:order-last">
 					<div className="w-full flex items-end justify-end">
 						<NavLink to="/user/CreatePlan">
-						<Button
-							variant="transparent"
-							className="font-nunito-sans font-semibold"
-						>
-							Create Plan
-							<Plus size={18} />
-						</Button>
+							<Button
+								variant="transparent"
+								className="font-nunito-sans font-semibold"
+							>
+								Create Plan
+								<Plus size={18} />
+							</Button>
 						</NavLink>
 					</div>
 
@@ -42,18 +157,20 @@ const HomeLayout = ({ children }) => {
 						<div className="w-full h-[1px] bg-[#B8BABD]"></div>
 					</div>
 
-					<div>{children}</div>
+					<div className="px-4 max-lg:mt-4">{children}</div>
 				</div>
 
-				<div className="col-span-2 pt-16 mx-10">
+				{/* My All Plans Section */}
+				<div className="lg:col-start-7 lg:col-span-2 max-lg:order-first lg:pt-16 lg:mx-10">
 					<h3 className="text-2xl font-semibold text-gray-700 mb-6 text-center">
 						My All Plans
 					</h3>
 					<div className="flex flex-col gap-4 py-2 px-4">
 						<Link to="/user">
 							<button
-							className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
-										pathname === "/user" ? "bg-white shadow-md"
+								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
+									pathname === "/user"
+										? "bg-white shadow-md"
 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
 								}`}
 							>
@@ -63,7 +180,8 @@ const HomeLayout = ({ children }) => {
 						<Link to="/user/published">
 							<button
 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
-										pathname === "/user/published" ? "bg-white shadow-md"
+									pathname === "/user/published"
+										? "bg-white shadow-md"
 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
 								}`}
 							>
@@ -73,7 +191,8 @@ const HomeLayout = ({ children }) => {
 						<Link to="/user/accepted">
 							<button
 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
-										pathname === "/user/accepted" ? "bg-white shadow-md"
+									pathname === "/user/accepted"
+										? "bg-white shadow-md"
 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
 								}`}
 							>
@@ -83,11 +202,11 @@ const HomeLayout = ({ children }) => {
 						<Link to="/user/favourite">
 							<button
 								className={`w-full text-center px-4 py-3 font-semibold rounded-md transition-colors cursor-pointer ${
-										pathname === "/user/favourite" ? "bg-white shadow-md"
+									pathname === "/user/favourite"
+										? "bg-white shadow-md"
 										: "bg-gray-200 text-gray-700 hover:bg-gray-300"
 								}`}
 							>
-
 								Favourite Agencies
 							</button>
 						</Link>
